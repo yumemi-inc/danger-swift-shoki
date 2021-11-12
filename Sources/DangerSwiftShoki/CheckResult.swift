4b9f22dacfa90b2e960228777706ad9c6781e269
史 翔新 <x_shi@yumemi.co.jp>
2021-11-13 01:48:06 +0900

    Add basic implementations of CheckResult

---
+++ Sources/DangerSwiftShoki/CheckResult.swift
@@ -0,0 +1,95 @@
+//
+//  CheckResult.swift
+//  
+//
+//  Created by 史 翔新 on 2020/07/11.
+//
+
+public struct CheckResult {
+    
+    public enum Result {
+        
+        case good
+        case acceptable
+        case rejected
+        
+        var markdownSymbol: String {
+            switch self {
+            case .good:
+                return ":tada:"
+                
+            case .acceptable:
+                return ":thinking:"
+                
+            case .rejected:
+                return ":no_good:"
+            }
+        }
+        
+    }
+    
+    typealias Message = (content: String, result: Result)
+    
+    public let title: String
+    
+    private var messages: [Message] = []
+    
+    private var todos: [String] = []
+    
+    public var warningsCount: Int {
+        messages.filter({ $0.result == .acceptable }).count
+    }
+    
+    public var errorsCount: Int {
+        messages.filter({ $0.result == .rejected }).count
+    }
+    
+    public init(title: String) {
+        self.title = title
+    }
+    
+    public mutating func askReviewer(to taskToDo: String) {
+        
+        todos.append(taskToDo)
+        
+    }
+    
+    public mutating func check(_ item: String, execution: () -> Result) {
+        
+        let result = execution()
+        messages.append((item, result))
+        
+    }
+    
+    public var markdownTitle: String {
+        
+        "## " + title
+        
+    }
+    
+    public var markdownMessage: String {
+        
+        let chartHeader = """
+            Checking Item | Result
+            | ---| --- |
+            
+            """
+        let chartContent = messages.map {
+            "\($0.content) | \($0.result.markdownSymbol)"
+        } .joined(separator: "\n")
+        
+        return chartHeader + chartContent
+        
+    }
+    
+    public var markdownTodos: String {
+        
+        let todoContent = todos.map {
+            "- [ ] \($0)"
+        }
+        
+        return todoContent.joined(separator: "\n")
+        
+    }
+    
+}
